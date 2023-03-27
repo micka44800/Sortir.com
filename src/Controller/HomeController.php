e59305bb5af4abfa039d6e579f1de44c99f7a4ad
arochedereux2022 <alexis.rochedereux2022@campus-eni.fr>
2023-03-27 16:22:49 +0200

    base

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,9 +10,12 @@ class HomeController extends AbstractController
 {
 
     #[Route('/home', name: 'app_home')]
-    public function aboutUs()
+    public function home()
     {
+        $lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
         return $this->render('main/home.html.twig', [
+
+            'lorem' => $lorem
 
         ]);
     }
