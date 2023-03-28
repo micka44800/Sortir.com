ba43544cfad31ba1013f4c240fca75f141a858f4
arochedereux2022 <alexis.rochedereux2022@campus-eni.fr>
2023-03-28 15:26:42 +0200

    base

--- src/Controller/monProfilController.php
+++ src/Controller/monProfilController.php
@@ -7,7 +7,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class monProfilController extends AbstractController
 {
-    #[Route('/profil', name: "app_monProfil")]
+    #[Route('/monProfil', name: "app_monProfil")]
     public function monProfil(){
         return $this->render('profil/monProfil.html.twig');
     }
