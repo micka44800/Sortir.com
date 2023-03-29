4d7cbc581f5d4317b780ec6a14fdddc2d3f12763
arochedereux2022 <alexis.rochedereux2022@campus-eni.fr>
2023-03-29 12:29:50 +0200

    base

--- src/Controller/MonProfilController.php
+++ src/Controller/MonProfilController.php
@@ -2,13 +2,24 @@
 
 namespace App\Controller ;
 
+use App\Entity\User;
+use App\Form\MonProfilFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MonProfilController extends AbstractController
 {
     #[Route('/monProfil', name: "app_monProfil")]
-    public function monProfil(){
-        return $this->render('profil/monProfil.html.twig');
+    public function monProfil(Request $request){
+
+        $user = new User();
+        $form = $this->createForm(MonProfilFormType::class, $user);
+        $form->handleRequest($request);
+
+        return $this->render('profil/monProfil.html.twig', [
+            'formMonProfil' => $form->createView(),
+        ]);
     }
+
 }
