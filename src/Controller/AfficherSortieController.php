8cac476c688fdb3502f735a20cc224c234e0c047
mchatry2022 <mickael.chatry@gmail.com>
2023-03-30 18:10:32 +0200

    afficher sortie

--- src/Controller/AfficherSortieController.php
+++ src/Controller/AfficherSortieController.php
@@ -2,17 +2,26 @@
 
 namespace App\Controller;
 
+use App\Form\InscriptionType;
+use App\Repository\SortieRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AfficherSortieController extends AbstractController
 {
     #[Route('/afficherSortie', name: 'app_afficherSortie')]
-    public function index(): Response
+    public function index( EntityManagerInterface $entityManager, SortieRepository $repository, Request $request): Response
     {
+
+        $sortie = $repository->findAll();
+
+
         return $this->render('afficher_sortie/afficherSortie.html.twig', [
-            'controller_name' => 'AfficherSortieController',
+            'SortieA' => $sortie,
         ]);
     }
 }
