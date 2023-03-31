0c6a2cc12e194601ed9238acc052ec27d6084f94
mchatry2022 <mickael.chatry@gmail.com>
2023-03-31 16:39:39 +0200

    afficher sortie et inscription a une sortie via le lien

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -22,6 +22,7 @@ class RegistrationFormType extends AbstractType
             ->add('nom')
             ->add('prenom')
             ->add('telephone')
+            ->add('site')
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
